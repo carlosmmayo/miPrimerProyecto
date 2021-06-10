80ffc6c7a5956d22306ab96cd0a7be1345ddb21c
carlosmmayo <lcmoreno701@misena.edu.co>
2021-06-10 17:40:04 -0500

    Creando controlador Cliente y agredando metodos Create, edit, Details y delete

---
+++ miPrimerProyecto/Controllers/ClienteController.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using miPrimerProyecto.Models;
+
+namespace miPrimerProyecto.Controllers
+{
+    public class ClienteController : Controller
+    {
+        // GET: Cliente
+        public ActionResult Index()
+        {
+            using (var db = new inventario2021Entities())
+            {
+                return View(db.cliente.ToList());
+            }
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(cliente cliente)
+        {
+
+            if (!ModelState.IsValid)
+                return View();
+            try
+            {
+                using (var db = new inventario2021Entities())
+                {
+                    db.cliente.Add(cliente);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "error " + ex);
+                return View();
+            }
+        }
+
+        public ActionResult Edit(int id)
+        {
+
+            try
+            {
+                using (var db = new inventario2021Entities())
+                {
+                    cliente findCliente = db.cliente.Where(b => b.id == id).FirstOrDefault();
+                    return View(findCliente);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "error " + ex);
+                return View();
+            }
+
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(cliente clienteEdit)
+        {
+
+            try
+            {
+                using (var db = new inventario2021Entities())
+                {
+                    cliente clie = db.cliente.Find(clienteEdit.id);
+                    clie.nombre = clienteEdit.nombre;
+                    clie.documento = clienteEdit.documento;
+                    clie.email = clienteEdit.email;
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "error " + ex);
+                return View();
+            }
+        }
+
+        public ActionResult Details(int id)
+        {
+
+            try
+            {
+                using (var db = new inventario2021Entities())
+                {
+                    cliente cliente = db.cliente.Find(id);
+                    return View(cliente);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "error " + ex);
+                return View();
+            }
+        }
+
+        public ActionResult Delete(int id)
+        {
+
+            try
+            {
+                using (var db = new inventario2021Entities())
+                {
+                    var cliente = db.cliente.Find(id);
+                    db.cliente.Remove(cliente);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "error " + ex);
+                return View();
+            }
+        }
+    }
+}
