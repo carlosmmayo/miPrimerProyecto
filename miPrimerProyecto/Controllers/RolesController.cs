b462cfcdb0cec958367caa21f58993a7cf9e3694
carlosmmayo <lcmoreno701@misena.edu.co>
2021-06-10 22:10:23 -0500

    Creando controlador Roles y agredando metodos Create, edit, Details y delete

---
+++ miPrimerProyecto/Controllers/RolesController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using miPrimerProyecto.Models;
+
+namespace miPrimerProyecto.Controllers
+{
+    public class RolesController : Controller
+    {
+        // GET: Roles
+        public ActionResult Index()
+        {
+            using (var db = new inventario2021Entities())
+            {
+                return View(db.roles.ToList());
+            }
+
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public ActionResult Create(roles roles)
+        {
+            if (!ModelState.IsValid)
+                return View();
+            try
+            {
+                using (var db = new inventario2021Entities())
+                {
+                    db.roles.Add(roles);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "error " + ex);
+                return View();
+            }
+        }
+
+        public ActionResult Edit(int id)
+        {
+
+            try
+            {
+                using (var db = new inventario2021Entities())
+                {
+                    roles findRoles = db.roles.Where(d => d.id == id).FirstOrDefault();
+                    return View(findRoles);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "error " + ex);
+                return View();
+            }
+
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(roles rolesEdit)
+        {
+
+            try
+            {
+                using (var db = new inventario2021Entities())
+                {
+                    roles role = db.roles.Find(rolesEdit.id);
+                    role.descripcion = rolesEdit.descripcion;
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "error " + ex);
+                return View();
+            }
+        }
+
+        public ActionResult Details(int id)
+        {
+
+            try
+            {
+                using (var db = new inventario2021Entities())
+                {
+                    roles role = db.roles.Find(id);
+                    return View(role);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "error " + ex);
+                return View();
+            }
+        }
+
+        public ActionResult Delete(int id)
+        {
+
+            try
+            {
+                using (var db = new inventario2021Entities())
+                {
+                    var roles = db.roles.Find(id);
+                    db.roles.Remove(roles);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "error " + ex);
+                return View();
+            }
+        }
+    }
+}
