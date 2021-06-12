857742b578f5646c872a4f75ebecc9045cd8b863
carlosmmayo <lcmoreno701@misena.edu.co>
2021-06-12 08:33:32 -0500

    Creando controlador Compra con llave a BD Foranea y agredando metodos Create, edit, Details, delete, nombreUusario y listarUsuarios, nombreCliente y lisarClientes  tambien se modifico vista Index, Create, Details,  Edit y se agrego layout listarUsarios y listarClientes del controlador Compra y se agrego el CRUD

---
+++ miPrimerProyecto/Controllers/CompraController.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using miPrimerProyecto.Models;
+
+namespace miPrimerProyecto.Controllers
+{
+    public class CompraController : Controller
+    {
+        // GET: Compra
+        public ActionResult Index()
+        {
+            using (var db = new inventario2021Entities())
+            {
+                return View(db.compra.ToList());
+            }
+
+        }
+
+        public static string nombreUsuario(int idUsuario)
+        {
+            using (var db = new inventario2021Entities())
+            {
+                return db.usuario.Find(idUsuario).nombre;
+            }
+        }
+
+        public ActionResult listarUsuarios()
+        {
+            using (var db = new inventario2021Entities())
+            {
+                return PartialView(db.usuario.ToList());
+            }
+        }
+
+        public static string nombreCliente(int idCliente)
+        {
+            using (var db = new inventario2021Entities())
+            {
+                return db.cliente.Find(idCliente).nombre;
+            }
+        }
+
+        public ActionResult listarClientes()
+        {
+            using (var db = new inventario2021Entities())
+            {
+                return PartialView(db.cliente.ToList());
+            }
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(compra newCompra)
+        {
+            if (!ModelState.IsValid)
+                return View();
+            try
+            {
+                using (var db = new inventario2021Entities())
+                {
+                    db.compra.Add(newCompra);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "error " + ex);
+                return View();
+            }
+        }
+
+        public ActionResult Details(int id)
+        {
+            using (var db = new inventario2021Entities())
+            {
+                compra compraDetalle = db.compra.Where(f => f.id == id).FirstOrDefault();
+                return View(compraDetalle);
+            }
+        }
+
+        public ActionResult Edit(int id)
+        {
+            try
+            {
+                using (var db = new inventario2021Entities())
+                {
+                    compra compra = db.compra.Where(f => f.id == id).FirstOrDefault();
+                    return View(compra);
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "error " + ex);
+                return View();
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(compra compraEdit)
+        {
+            try
+            {
+                using (var db = new inventario2021Entities())
+                {
+                    var compr = db.compra.Find(compraEdit.id);
+                    compr.fecha = compraEdit.fecha;
+                    compr.total = compraEdit.total;
+                    compr.id_usuario = compraEdit.id_usuario;
+                    compr.id_cliente = compraEdit.id_cliente;
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "error " + ex);
+                return View();
+            }
+        }
+
+        public ActionResult Delete(int id)
+        {
+
+            try
+            {
+                using (var db = new inventario2021Entities())
+                {
+                    var compra = db.compra.Find(id);
+                    db.compra.Remove(compra);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "error " + ex);
+                return View();
+            }
+        }
+    }
+}
